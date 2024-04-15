11e57c7edb68f13e0aa0aa7dc63c832ffb4f7e0e
Gopi Kishore <kishorepenjendra@gmail.com>
2024-04-15 15:38:06 +0530

    fix: 🐛 null check fix

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -15,211 +15,213 @@ function Chart({ selectedItem }) {
   const [toggle1, setToggle1] = useState(true);
   const [toggle2, setToggle2] = useState(true);
   useLayoutEffect(() => {
-    let root = am5.Root.new("chartdiv");
+    if (Object.keys(selectedItem)?.length > 0) {
+      let root = am5.Root.new("chartdiv");
 
-    const myTheme = am5.Theme.new(root);
+      const myTheme = am5.Theme.new(root);
 
-    myTheme.rule("AxisLabel").setAll({
-      fill: am5.color(0x7b7f80),
-      fontSize: "0.75rem",
-      textAlign: "center",
-    });
+      myTheme.rule("AxisLabel").setAll({
+        fill: am5.color(0x7b7f80),
+        fontSize: "0.75rem",
+        textAlign: "center",
+      });
 
-    root.setThemes([myTheme, am5themes_Animated.new(root)]);
+      root.setThemes([myTheme, am5themes_Animated.new(root)]);
 
-    let chart = root.container.children.push(
-      am5xy.XYChart.new(root, {
-        // panY: false,
-        layout: root.verticalLayout,
-      })
-    );
+      let chart = root.container.children.push(
+        am5xy.XYChart.new(root, {
+          // panY: false,
+          layout: root.verticalLayout,
+        })
+      );
 
-    // Define data
+      // Define data
 
-    // Create Y-axis
-    let yAxis = chart.yAxes.push(
-      am5xy.ValueAxis.new(root, {
-        renderer: am5xy.AxisRendererY.new(root, {
-          strokeOpacity: 1,
-          strokeWidth: 2,
-          stroke: am5.color(0x406a8d),
-          minGridDistance: 75,
-        }),
-      })
-    );
+      // Create Y-axis
+      let yAxis = chart.yAxes.push(
+        am5xy.ValueAxis.new(root, {
+          renderer: am5xy.AxisRendererY.new(root, {
+            strokeOpacity: 1,
+            strokeWidth: 2,
+            stroke: am5.color(0x406a8d),
+            minGridDistance: 75,
+          }),
+        })
+      );
 
-    // Create X-Axis
-    let xAxis = chart.xAxes.push(
-      am5xy.CategoryAxis.new(root, {
-        renderer: am5xy.AxisRendererX.new(root, {
-          strokeOpacity: 1,
-          strokeWidth: 2,
-          stroke: am5.color(0x406a8d),
-          minGridDistance: 1,
-        }),
-        categoryField: "category",
-      })
-    );
-    xAxis.data.setAll(selectedItem?.data);
+      // Create X-Axis
+      let xAxis = chart.xAxes.push(
+        am5xy.CategoryAxis.new(root, {
+          renderer: am5xy.AxisRendererX.new(root, {
+            strokeOpacity: 1,
+            strokeWidth: 2,
+            stroke: am5.color(0x406a8d),
+            minGridDistance: 1,
+          }),
+          categoryField: "category",
+        })
+      );
+      xAxis.data.setAll(selectedItem?.data);
 
-    let xRenderer = xAxis.get("renderer");
-    xRenderer.grid.template.setAll({
-      stroke: am5.color(0x406a8d),
-      strokeWidth: 1,
-      strokeOpacity: 0.5,
-    });
-    xRenderer.grid.template.setAll({
-      location: 0.5,
-    });
+      let xRenderer = xAxis.get("renderer");
+      xRenderer.grid.template.setAll({
+        stroke: am5.color(0x406a8d),
+        strokeWidth: 1,
+        strokeOpacity: 0.5,
+      });
+      xRenderer.grid.template.setAll({
+        location: 0.5,
+      });
 
-    xRenderer.labels.template.setAll({});
+      xRenderer.labels.template.setAll({});
 
-    let yRenderer = yAxis.get("renderer");
+      let yRenderer = yAxis.get("renderer");
 
-    // Create series
-    let tooltip = am5.Tooltip.new(root, {
-      getFillFromSprite: false,
-      labelText: "[bold]{categoryX}[/]\n: {valueY}",
-    });
+      // Create series
+      let tooltip = am5.Tooltip.new(root, {
+        getFillFromSprite: false,
+        labelText: "[bold]{categoryX}[/]\n: {valueY}",
+      });
 
-    tooltip.get("background").setAll({
-      fill: am5.color(0xffffff),
-      fillOpacity: 0.8,
-    });
+      tooltip.get("background").setAll({
+        fill: am5.color(0xffffff),
+        fillOpacity: 0.8,
+      });
 
-    let series1 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value1",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(legendData[0].colorHex),
-      })
-    );
-    let series2 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value2",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(legendData[1].colorHex),
-      })
-    );
-    let series3 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value3",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(0x79c0ff),
-      })
-    );
-    let series4 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value4",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(legendDataForcast[0].colorHex),
-      })
-    );
-    let series5 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value5",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(legendDataForcast[1].colorHex),
-      })
-    );
-    let series6 = chart.series.push(
-      am5xy.LineSeries.new(root, {
-        name: "Series",
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: "value6",
-        categoryXField: "category",
-        locationX: 0.5,
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "[bold]{categoryX}[/]\n: {valueY}",
-        }),
-        stroke: am5.color(legendDataForcast[2].colorHex),
-      })
-    );
-    series1.data.setAll(selectedItem?.data);
-    series2.data.setAll(selectedItem?.data);
-    series3.data.setAll(selectedItem?.data);
-    series4.data.setAll(selectedItem?.data);
-    series5.data.setAll(selectedItem?.data);
-    series6.data.setAll(selectedItem?.data);
-    series4.strokes.template.setAll({
-      strokeWidth: 1,
-      strokeDasharray: [10, 5],
-    });
-    series5.strokes.template.setAll({
-      strokeWidth: 1,
-      strokeDasharray: [10, 5],
-    });
-    series6.strokes.template.setAll({
-      strokeWidth: 1,
-      strokeDasharray: [10, 5],
-    });
+      let series1 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value1",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(legendData[0].colorHex),
+        })
+      );
+      let series2 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value2",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(legendData[1].colorHex),
+        })
+      );
+      let series3 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value3",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(0x79c0ff),
+        })
+      );
+      let series4 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value4",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(legendDataForcast[0].colorHex),
+        })
+      );
+      let series5 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value5",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(legendDataForcast[1].colorHex),
+        })
+      );
+      let series6 = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          name: "Series",
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: "value6",
+          categoryXField: "category",
+          locationX: 0.5,
+          tooltip: am5.Tooltip.new(root, {
+            labelText: "[bold]{categoryX}[/]\n: {valueY}",
+          }),
+          stroke: am5.color(legendDataForcast[2].colorHex),
+        })
+      );
+      series1.data.setAll(selectedItem?.data);
+      series2.data.setAll(selectedItem?.data);
+      series3.data.setAll(selectedItem?.data);
+      series4.data.setAll(selectedItem?.data);
+      series5.data.setAll(selectedItem?.data);
+      series6.data.setAll(selectedItem?.data);
+      series4.strokes.template.setAll({
+        strokeWidth: 1,
+        strokeDasharray: [10, 5],
+      });
+      series5.strokes.template.setAll({
+        strokeWidth: 1,
+        strokeDasharray: [10, 5],
+      });
+      series6.strokes.template.setAll({
+        strokeWidth: 1,
+        strokeDasharray: [10, 5],
+      });
 
-    if (!toggle1) {
-      series1.hide();
-      series4.hide();
+      if (!toggle1) {
+        series1.hide();
+        series4.hide();
+      }
+      if (!toggle2) {
+        series2.hide();
+        series5.hide();
+      }
+      // Add legend
+      // let legend = chart.children.push(am5.Legend.new(root, {}));
+      // legend.data.setAll(chart.series.values);
+
+      // Add cursor
+      let cursor = chart.set(
+        "cursor",
+        am5xy.XYCursor.new(root, {
+          behavior: "selectX",
+        })
+      );
+      // let cursor = chart.set(
+      //   "cursor",
+      //   am5xy.XYCursor.new(root, {
+      //     behavior: "none",
+      //   })
+      // );
+      cursor.lineY.set("visible", false);
+
+      return () => {
+        root.dispose();
+      };
     }
-    if (!toggle2) {
-      series2.hide();
-      series5.hide();
-    }
-    // Add legend
-    // let legend = chart.children.push(am5.Legend.new(root, {}));
-    // legend.data.setAll(chart.series.values);
-
-    // Add cursor
-    let cursor = chart.set(
-      "cursor",
-      am5xy.XYCursor.new(root, {
-        behavior: "selectX",
-      })
-    );
-    // let cursor = chart.set(
-    //   "cursor",
-    //   am5xy.XYCursor.new(root, {
-    //     behavior: "none",
-    //   })
-    // );
-    cursor.lineY.set("visible", false);
-
-    return () => {
-      root.dispose();
-    };
   }, [toggle1, toggle2, selectedItem]);
 
   return (
